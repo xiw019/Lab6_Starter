ab110795045b34a7b2cd74631024cc461cfd8ee5
xiw019 <xiw019@gmail.com>
2021-10-31 19:25:31 -0700

    updated

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -95,8 +95,7 @@ class RecipeCard extends HTMLElement {
       // Part 1 Expose - TODO
       
       //img
-      //this.shadowRoot.appendChild(styleElem);
-      shadowRoot.appendChild(styleElem);
+      this.shadowRoot.appendChild(styleElem);
       let image = document.createElement('img');
       image.src = searchForKey(data, 'thumbnailUrl');
       image.alt = searchForKey(data, 'headline');
